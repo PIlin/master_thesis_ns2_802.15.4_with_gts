23b2661cc99c99f32ea15059275e38d2233f7c8c
pashkoff <pashkoff2@gmail.com>
2013-11-03 03:08:26 +0200

    add path setup script

---
+++ setup_ns2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+export PATH="/home/pashkoff/code/ns-allinone-2.35/bin:/home/pashkoff/code/ns-allinone-2.35/tcl8.5.10/unix:/home/pashkoff/code/ns-allinone-2.35/tk8.5.10/unix:${PATH}"
+
+export LD_LIBRARY_PATH="/home/pashkoff/code/ns-allinone-2.35/otcl-1.14:/home/pashkoff/code/ns-allinone-2.35/lib;${LD_LIBRARY_PATH}"
+
+export TCL_LIBRARY="/home/pashkoff/code/ns-allinone-2.35/tcl8.5.10/library:/home/pashkoff/code/ns-allinone-2.35/tk8.5.10/library"
+
+
+
