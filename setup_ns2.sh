f741c26b694f4abb18c0961c62199218caf6a531
pashkoff <pashkoff2@gmail.com>
2013-11-04 23:33:25 +0200

    fix environment setup script

--- setup_ns2.sh
+++ setup_ns2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-export PATH="/home/pashkoff/code/ns-allinone-2.35/bin:/home/pashkoff/code/ns-allinone-2.35/tcl8.5.10/unix:/home/pashkoff/code/ns-allinone-2.35/tk8.5.10/unix:${PATH}"
+export PATH="/home/pashkoff/code/ns-allinone/bin:/home/pashkoff/code/ns-allinone/tcl8.5.10/unix:/home/pashkoff/code/ns-allinone/tk8.5.10/unix:${PATH}"
 
-export LD_LIBRARY_PATH="/home/pashkoff/code/ns-allinone-2.35/otcl-1.14:/home/pashkoff/code/ns-allinone-2.35/lib;${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="/home/pashkoff/code/ns-allinone/otcl-1.14:/home/pashkoff/code/ns-allinone/lib;${LD_LIBRARY_PATH}"
 
-export TCL_LIBRARY="/home/pashkoff/code/ns-allinone-2.35/tcl8.5.10/library:/home/pashkoff/code/ns-allinone-2.35/tk8.5.10/library"
+export TCL_LIBRARY="/home/pashkoff/code/ns-allinone/tcl8.5.10/library:/home/pashkoff/code/ns-allinone/tk8.5.10/library"
 
 
 
